1ff0f0ceb694622acb96dd81cc09aef8003249d8
Guillaume Monrolin <guillaume.monrolin@crocogiciel.com>
2025-02-26 23:06:01 +0530

    Add the PWA configuration

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,22 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('my-cache').then((cache) => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/offline.html', // Offline access page
-          // Add other static files here
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((response) => {
-        return response || fetch(e.request);
-      })
-    );
-  });
+self.addEventListener('push', function (event) {
+    if (event.data) {
+      const data = event.data.json()
+      const options = {
+        body: data.body,
+        icon: data.icon || '/icon.png',
+        badge: '/badge.png',
+        vibrate: [100, 50, 100],
+        data: {
+          dateOfArrival: Date.now(),
+          primaryKey: '2',
+        },
+      }
+      event.waitUntil(self.registration.showNotification(data.title, options))
+    }
+  })
+   
+  self.addEventListener('notificationclick', function (event) {
+    console.log('Notification click received.')
+    event.notification.close()
+    event.waitUntil(clients.openWindow('<https://lenz-up.vercel.app>'))
+  })
